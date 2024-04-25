3714d9c2f6f77c44cdd05399a36f27078149396d
Daniel Chipolesk <168142021+Chipolesk@users.noreply.github.com>
2024-04-25 19:27:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function site1() {
+    window.location.href = 'http://t1342a24n09.wuaze.com/site1'
+
+}
+function site2() {
+    window.location.href = 'http://t1342a24n09.wuaze.com/site2'
+}
+function site3() {
+    window.location.href = 'http://t1342a24n09.wuaze.com/site3'
+}
+function adm() {
+    window.location.href = ' https://mail.google.com/mail/?view=cm&fs=1&to=danielchipolesk@gmail.com'
+}
